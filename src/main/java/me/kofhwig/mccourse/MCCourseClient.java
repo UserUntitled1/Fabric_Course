1a6659ec22e1b326eed702dae67b5e66e363646d
UserUntitled1 <qwuhfe9urgnvwrgne8prg@gmail.com>
2025-11-09 16:53:58 +0100

    Initial commit

---
+++ src/main/java/me/kofhwig/mccourse/MCCourseClient.java
@@ -0,0 +1,10 @@
+package me.kofhwig.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
